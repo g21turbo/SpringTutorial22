ee3bf762012e0e242be6a7f67eca81f74ac55141
User <dg21stew@gmail.com>
2022-11-19 17:03:48 -0800

    update and delete methods

--- src/main/java/com/example/SpringTutorial22/Student/StudentRepository.java
+++ src/main/java/com/example/SpringTutorial22/Student/StudentRepository.java
@@ -1,9 +1,18 @@
 package com.example.SpringTutorial22.Student;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository // means this interface is responsible for data access
 public interface StudentRepository
         extends JpaRepository<Student, Long> {
+
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
+
+
 }
